a89e1224415f6ce80586269458f0370c916919e5
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2016-09-23 14:45:59 -0400

    Developing MVP with Users, Posts & Comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,55 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
-		@posts = Post.all.order("created_by DESC")
+		@posts = Post.all.order("created_at DESC")
 	end
 
+
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		if @post.save
 			redirect_to root_path
 		else
 			render 'new'
 		end
 	end
+
+	def show
+	
+	end
 		
+	def update
+		if @post.update(post_params)
+			redirect_to post_path
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+	
+	end
+
+	def destroy
+		if @post.destroy()
+			redirect_to root_path
+		end
+	end
 
 
 	private
 
 	def post_params
-		params.require(:post).permit(:content, :user_id)
+		params.require(:post).permit(:content, :user_id, :created_by, :profile_id)
 	end
 
+	def find_post
+		@post = Post.find(params[:id])
+	end
 end
